91e6072c124cf0a623cc2f5c19a341730e52fe09
DESKTOP-O0K5VUL\Darron <darrenhoun@gmail.com>
2019-01-26 01:35:02 -0800

    adding cat behavior scripts

---
+++ GGJ2019/Assets/Scripts/catBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class catBehavior : MonoBehaviour
+{
+
+    [SerializeField]
+    private int range = 3;
+
+    void Awake (){
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+
+    //Vector3Int getPriority(){
+        
+    //}
+
+    void move(){
+        
+    }
+}
