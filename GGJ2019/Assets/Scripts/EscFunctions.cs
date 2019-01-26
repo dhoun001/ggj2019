0e3e21d41badac115de96db752fefb5b4eeed57d
Rica Feng <rfeng001@ucr.edu>
2019-01-26 11:10:49 -0800

    escape script used for quitting game quickly

---
+++ GGJ2019/Assets/Scripts/EscFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscFunctions : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            MenuFunctions.Instance.QuitGame();
+        }
+    }
+}
