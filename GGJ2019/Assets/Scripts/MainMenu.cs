52825cfd90f580be360b8642e261d1658f42d6a5
Rica Feng <rfeng001@ucr.edu>
2019-01-26 10:46:05 -0800

    main menu scene and functionality

---
+++ GGJ2019/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _mainMenuScreen;
+
+    [SerializeField]
+    private GameObject _choosePetScreen;
+
+    public void ChoosePetName()
+    {
+        _mainMenuScreen.gameObject.SetActive(false);
+        _choosePetScreen.gameObject.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        _mainMenuScreen.gameObject.SetActive(true);
+        _choosePetScreen.gameObject.SetActive(false);
+    }
+
+    public void LoadLevel()
+    {
+        MenuFunctions.Instance.LoadNextLevel();
+    }
+
+    public void QuitGame()
+    {
+        MenuFunctions.Instance.QuitGame();
+    }
+}
---
+++ GGJ2019/Assets/Scripts/MenuFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuFunctions : Singleton<MenuFunctions>
+{
+    [SerializeField]
+    private int _maxLevel = 3;
+
+    /// <summary>
+    /// Load the next scene level. Don't use on the last level (max level), instead transition to game win scene
+    /// </summary>
+    public void LoadNextLevel()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        int levelNumber = 1;
+        System.Int32.TryParse(sceneName[sceneName.Length - 1].ToString(), out levelNumber);
+        string levelSceneName = "Level" + levelNumber.ToString();
+
+        SceneManager.LoadScene("Level" + levelNumber);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
