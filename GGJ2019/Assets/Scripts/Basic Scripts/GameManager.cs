75e8a7454b314607aa79ab239568ccd5681de32f
Rica Feng <rfeng001@ucr.edu>
2019-01-26 10:59:32 -0800

    method stubs in game manager

--- GGJ2019/Assets/Scripts/Basic Scripts/GameManager.cs
+++ GGJ2019/Assets/Scripts/Basic Scripts/GameManager.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : Singleton<GameManager> {
+public class GameManager : Singleton<GameManager>
+{
+    public string petName = "Pet Name";
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void StartRun()
+    {
+        //TODO: disable inventory
+        //TODO: start cat movement
+    }
+
+    public void RestartRun()
+    {
+        //TODO: return all inventory items to hotbar
+        //TODO: Reset cat to starting position
+        //TODO: reset all other prop data
+    }
 }
