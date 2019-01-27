319b3aebc534ea68f5b4ee80fc6f59940fc34baa
Rica Feng <rfeng001@ucr.edu>
2019-01-27 14:35:20 -0800

    asdasd

--- GGJ2019/Assets/Scripts/Vase.cs
+++ GGJ2019/Assets/Scripts/Vase.cs
@@ -10,7 +10,7 @@ public class Vase : gridItem
         UIController.Instance.ShowLoseMessage();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "cat")
         {
