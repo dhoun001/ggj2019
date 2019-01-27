45039828f79a8af5b12acb6473e9b47139b53de3
Rica Feng <rfeng001@ucr.edu>
2019-01-26 17:46:49 -0800

    added assets to level 1 added sorting order script

--- GGJ2019/Assets/Scripts/UIController.cs
+++ GGJ2019/Assets/Scripts/UIController.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : Singleton<UIController>
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Image _fadeImage;
+
+    public void ShowWinMessage()
     {
-        
+        Color color = _fadeImage.color;
+        color.a = 0.5f;
+        _fadeImage.color = color;
+        //TODO: show win messsage
+        //TODO: show next level button, quit to menu button
+        //TODO: show good feelings
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowLoseMessage()
     {
-        
+        //TODO: show lose message
+        //TODO: flash Restart button
+        //TODO: at this point, ensure player cannot win level unless they restart
     }
 }
