b941ded09fae68fc2567938476bf0d95599765f1
Rica Feng <rfeng001@ucr.edu>
2019-01-26 16:11:33 -0800

    new art assets, brushes, uicontroller

---
+++ GGJ2019/Assets/Scripts/Vase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vase : gridItem
+{
+    public override void OnArrive()
+    {
+        //TODO: lose level
+    }
+}
